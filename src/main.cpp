c3cb058d90b984a3e9beaa01b194154426ba5c87
CFieldhouse <timpat1111@gmail.com>
2021-12-18 19:38:32 +0000

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#define DAC1 25
+#define LED 2
+
+#include "song.h"
+
+void setup() { 
+  Serial.begin(115200);
+  // initialize EEPROM with predefined size
+  pinMode(LED, OUTPUT);
+  pinMode(DAC1, OUTPUT);
+}
+
+void loop() {
+  //digitalWrite(LED, HIGH);
+  for (long i = 0; i < song_len; ++i){
+    dacWrite(DAC1, song[i]);
+    delayMicroseconds(60);
+  }
+}
---
+++ write_wav.py
@@ -0,0 +1,34 @@
+from scipy.io import wavfile
+import serial
+import numpy as np
+from tqdm import tqdm
+
+freq, arr = wavfile.read("jingle16.wav")
+
+arr = arr.astype("float")
+arr = arr / np.std(arr)
+
+def s(sig = 2.5):
+    # esp reads 255 by default or something, so can't use it
+    return (254 * np.clip(arr + sig, 0, 2*sig) / (2*sig)).astype("int")
+
+s8 = [t[0] for t in s(5)]
+
+N = 1000000
+b = 3
+with open("src/song.h", "w") as f:
+    f.write("//#include <Arduino.h>\n\n")
+    f.write(f"#define song_len {N}\n")
+    f.write("const uint8_t PROGMEM song[song_len] {\n")
+    for i in range(N-1):
+        f.write(str(s8[i*b])+", ")
+        if i%1500 == 0:
+            f.write("\n")
+    f.write(str(s8[(N-1)*b])+"\n};")
+
+#ser = serial.Serial("/dev/ttyUSB0", baudrate=115200)
+#for i in tqdm(range(200000)):
+#    byte = int(s8[2*i]).to_bytes(1, byteorder="big")
+#    ser.write(b'*')
+#    ser.write(byte)
+#    print(ser.read_until(b'\n'))
